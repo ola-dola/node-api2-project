d5cb3a70f43536e69bcb2a331dc8292810011992
ola-dola <oredolaolamide@gmail.com>
2020-01-14 16:26:15 +0100

    wip: api serving post comments

--- data/db-router.js
+++ data/db-router.js
@@ -4,6 +4,7 @@ const DbHelpers = require("./db");
 
 const router = express.Router();
 
+//get all posts
 router.get("/", (req, res) => {
   DbHelpers.find()
     .then(data => {
@@ -16,23 +17,45 @@ router.get("/", (req, res) => {
     });
 });
 
+//Get posts by id
 router.get("/:id", (req, res) => {
   const { id } = req.params;
 
   DbHelpers.findById(id)
     .then(data => {
-      if (data.length === 0) {
+      if (data.length > 0) {
+        res.status(200).json(data);
+      } else {
         res
           .status(404)
           .json({ message: "The post with the specified ID does not exist." });
-      } else {
-        res.status(200).json(data);
       }
     })
     .catch(error => {
       res
         .status(500)
         .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+//all comments for a post
+router.get("/:id/comments", (req, res) => {
+  const { id } = req.params;
+
+  DbHelpers.findPostComments(id)
+    .then(data => {
+      if (data.length > 0) {
+        res.status(200).json(data);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The comments information could not be retrieved." });
     });
 });
 
